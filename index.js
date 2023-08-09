791293e5b884be60f1418a0c2d09e5782261fdf5
“Elena <kisek76@mail.ru>
2023-08-09 14:14:49 -0400

    add second product

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const product = 'Apple Watch Ultra';
-const quantity = 2;
-const category = 'Watch';
-const price = 1099;
+"use strict"
 
-console.log(`Общая сумма: ${quantity * price}`);
+{
+    const product = "Apple Watch Ultra";
+    const quantity = 2;
+    const category = "Watch";
+    const price = 1099;
+
+    console.log(`Общая сумма: ${quantity * price}`);
+}
+
+{
+  const product = "Apple Cortland";
+  const quantity = 5;
+  const category = "food";
+  const price = 8;
+
+  console.log(`Общая сумма: ${quantity * price}`);
+}
