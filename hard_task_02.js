fd967be01a06c494f838e3e08633e319027e7648
“Elena <kisek76@mail.ru>
2023-08-13 17:38:10 -0400

    second hard task not completed

--- hard_task_02.js
+++ hard_task_02.js
@@ -1,12 +1,12 @@
-let num1 = +prompt("Введите первое число: ");
-let num2 = +prompt("Введите второе число: ");
+const num1 = +prompt("Введите первое число: ");
+const num2 = +prompt("Введите второе число: ");
 
 const minNumber = (num1, num2) => {
-    if (Number.isInteger(num1) || Number.isInteger(num2)) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        console.log('Введите число!');
+    } else {
         if (num1 < num2) return num1;
         return num2;
-    } else {
-        console.log(('Введите число!'));
     }
 }
 console.log(minNumber(num1, num2));
