1a039641defc81e27ee8863512fe96219f0185e3
“Elena <kisek76@mail.ru>
2023-08-13 08:32:53 -0400

    first hard task fixed

--- tasksHard_1.js
+++ tasksHard_1.js
@@ -1,11 +1,17 @@
-const salary = +prompt("Какая ваша заработная плата?");
+const lowTax = 0.13;
+const middleTax = 0.2;
+const highTax = 0.3;
+const firstLimit = 15000;
+const secondLimit = 50000;
 
-if (salary >= 50000) {
-  console.log(`Ваш налог составляет ${salary * 0.3} от полученого дохода`);
-} else if (salary >= 15000) {
-  console.log(`Ваш налог составляет ${salary * 0.2} от полученого дохода`);
+let salary = +prompt("Какая ваша заработная плата?");
+
+if (salary >= secondLimit) {
+  console.log(`Ваш налог составляет ${salary * highTax} от полученого дохода`);
+} else if (salary >= firstLimit) {
+  console.log(`Ваш налог составляет ${salary * middleTax} от полученого дохода`);
 } else if (salary >= 0) {
-  console.log(`Ваш налог составляет ${salary * 0.13} от полученого дохода`);
+  console.log(`Ваш налог составляет ${salary * lowTax} от полученого дохода`);
 } else {
-  console.log(`Ваш налог составляет 0`);
+  console.log('Ваш налог составляет 0 от полученого дохода');
 }
