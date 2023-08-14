9d9a7054021c07bfce398a218cf43c058123527d
“Elena <kisek76@mail.ru>
2023-08-14 11:38:54 -0400

    first task completed

--- task01.js
+++ task01.js
@@ -12,18 +12,7 @@ const failedStudents = ["Сидоров", "Смирнов", "Попов"];
 let passedStudents = [];
 
 const filter = () => {
-
-    for (let i = 0; i < failedStudents.length; i++) {
-        for (let j = 0; j < failedStudents.length; j++) {
-            if (allStudents[i] !== failedStudents[j]) {
-                passedStudents.push(allStudents[i])
-            }
-        }
-
-    }
-            return passedStudents;
-    
-
+    return passedStudents = allStudents.filter(i => failedStudents.indexOf(i) < 0)
 }
 
 console.log(filter());
